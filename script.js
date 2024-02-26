9979309cc38a47b80253e62be3365a6b6f1b8ef6
Alexeev <Mikhail.Alexeev@bsci.com>
2024-02-26 22:31:40 +0300

    1st comm

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("site1Btn").addEventListener("click", function() {
+    window.location.href = "http://85.234.106.147/tarrifs";
+});
+
+document.getElementById("site2Btn").addEventListener("click", function() {
+    window.location.href = "https://boston.catzwolf.ru/";
+});
